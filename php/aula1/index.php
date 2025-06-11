736df725bccd4f2bd453da91fc0ad7c422279d44
JAMES DONIZETE SGANZERLA FELIPE <james.dsfelipe@senacsp.edu.br>
2025-06-11 11:22:26 -0300

    aula1

--- php/aula1/index.php
+++ php/aula1/index.php
@@ -1,12 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
 <title>Exemplo </title>
 </head>
 <body>
 
 <?php
+# primeiro código
 echo "Olá Mundo!";
+//imprime a mensagem Olá Mundo na tela
+/* comentários em linha podem ser feitos com os símbolos # e //, já os comentários com mais de uma linha devem ser feitos com os sinais /* e */
+//phpinfo ();
+
+//Variáveis em PHP
+
+$texto = " <h1> Sou aluno da TI36 no Senac de Botucatu </h1>";
+echo $texto;
+
+$divisao = "<br>============================================== </br>";
+echo $divisao;
+$nome = "Chaves";
+$sobrenome = "del Ocho";
+echo " Olá, meu nome é $nome e meu sobrenome é $sobrenome.";
+/*Ex. 1 - Crie um script PHP que declare três variáveis: $nome, $idade e $cidade. Atribua valores a essas variáveis e exiba uma mensagem que combine esses valores. */
+$divisao = "<br>============================================== </br>";
+echo $divisao;
+echo $nome = "James";
+echo $idade = "33anos";
+echo $cidade = "Botucatu";
+
+echo " Olá meu nome é $nome, tenho $idade e sou da cidade de $cidade.";
+
+
 ?>
 
 </body>
